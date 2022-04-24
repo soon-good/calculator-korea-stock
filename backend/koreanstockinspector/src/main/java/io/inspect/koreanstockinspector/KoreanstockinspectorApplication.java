a22aba179863e57fe3d60770d96a75c523d94d82
soon.good <playmymind@daum.net>
2022-04-25 05:03:28 +0900

    initial code

---
+++ backend/koreanstockinspector/src/main/java/io/inspect/koreanstockinspector/KoreanstockinspectorApplication.java
@@ -0,0 +1,13 @@
+package io.inspect.koreanstockinspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanstockinspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreanstockinspectorApplication.class, args);
+	}
+
+}
---
+++ backend/koreanstockinspector/src/test/java/io/inspect/koreanstockinspector/KoreanstockinspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.inspect.koreanstockinspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreanstockinspectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
