f018a8eb09656a74d4626469ca317b8ac8c2cb6a
soon.good <playmymind@daum.net>
2022-05-06 15:46:53 +0900

    DynamoDBMapper 를 이용한 테이블 삭제 테스트 코드 추가

--- backend/koreanstockinspector/src/test/java/io/inspect/koreanstockinspector/tdd/finance/repository/dynamo/LocalDynamoFinanceYearlyCRUDTest.java
+++ backend/koreanstockinspector/src/test/java/io/inspect/koreanstockinspector/tdd/finance/repository/dynamo/LocalDynamoFinanceYearlyCRUDTest.java
@@ -8,17 +8,18 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 import io.inspect.koreanstockinspector.finance.domain.FinanceGainLossYearly;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class LocalDynamoFinanceYearlyCRUDTest {
-
     AmazonDynamoDB amazonDynamoDB;
     DynamoDBMapper dynamoDBMapper;
 
@@ -26,7 +27,7 @@ public class LocalDynamoFinanceYearlyCRUDTest {
     public void testLocalInit(){
         AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
                 "http://localhost:5555",
-                "ap-northeast-2"
+                "local"
         );
 
         AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(
@@ -44,8 +45,10 @@ public class LocalDynamoFinanceYearlyCRUDTest {
         dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB, DynamoDBMapperConfig.DEFAULT);
     }
 
+    // AmazonDynamoDB SDK 를 사용하지 않고 DynamodbMapper 를 이용해 테이블을 생성
+    @Order(1)
     @Test
-    @DisplayName("AmazonDynamoDB SDK 가 아닌, spring-data-dynamodb 의 DynamodbMapper 를 이용해 테이블을 생성해본다.")
+    @DisplayName("FinanceGainLossYearly 테이블 생성 >> spring-data-dynamodb 의 DynamodbMapper 를 이용해 테이블을 생성")
     public void TEST_CRAETE_TABLE_BY_ORM_MAPPER_DYNAMODBMAPPER(){
         CreateTableRequest request = dynamoDBMapper.generateCreateTableRequest(FinanceGainLossYearly.class)
                 .withProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
@@ -53,4 +56,13 @@ public class LocalDynamoFinanceYearlyCRUDTest {
         then(TableUtils.createTableIfNotExists(amazonDynamoDB, request)).isTrue();
     }
 
+
+    // AmazonDynamoDB SDK 를 사용하지 않고 DynamodbMapper 를 이용해 테이블을 삭제
+    @Order(2)
+    @Test
+    @DisplayName("FinanceGainLossYearly 테이블 삭제 >> spring-data-dynamodb 의 DynamodbMapper 를 이용해 테이블을 삭제")
+    public void TEST_DELETE_TABLE_BY_ORM_MAPPER_DYNAMOMAPPER(){
+        DeleteTableRequest deleteTableRequest = dynamoDBMapper.generateDeleteTableRequest(FinanceGainLossYearly.class);
+        then(TableUtils.deleteTableIfExists(amazonDynamoDB, deleteTableRequest)).isTrue();
+    }
 }
