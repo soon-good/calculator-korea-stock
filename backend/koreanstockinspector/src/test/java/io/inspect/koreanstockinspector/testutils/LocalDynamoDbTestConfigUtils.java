5f345c750c95e916c0252b7abca1273a5d1459cb
soon.good <playmymind@daum.net>
2022-05-06 16:13:45 +0900

    임시로 사용할 DynamoDB, AmazonDynamoDB 설정 코드 추가

---
+++ backend/koreanstockinspector/src/test/java/io/inspect/koreanstockinspector/testutils/LocalDynamoDbTestConfigUtils.java
@@ -0,0 +1,36 @@
+package io.inspect.koreanstockinspector.testutils;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+
+public class LocalDynamoDbTestConfigUtils {
+    record LocalDynamoDbEnvironment(AmazonDynamoDB amazonDynamoDB, DynamoDBMapper dynamoDBMapper){}
+
+    public static LocalDynamoDbEnvironment newLocalDynamoDbEnvironment(){
+        AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
+                "http://localhost:5555",
+                "local"
+        );
+
+        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(
+                        "type the key",
+                        "type the key"
+                )
+        );
+
+        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+                .withRegion("ap-northeast-2")
+                .withCredentials(credentialsProvider)
+                .build();
+
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB, DynamoDBMapperConfig.DEFAULT);
+
+        return new LocalDynamoDbEnvironment(amazonDynamoDB, dynamoDBMapper);
+    }
+}
