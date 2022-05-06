962998fd69ea9b1431736f077591696e083d62f1
soon.good <playmymind@daum.net>
2022-05-07 00:22:07 +0900

    PriceDayKr 테이블 매핑 추가 및 테이블 생성 테스트코드

--- backend/koreanstockinspector/src/test/java/io/inspect/koreanstockinspector/tdd/price/domain/LocalDynamoPriceKrDayCRUDTest.java
+++ backend/koreanstockinspector/src/test/java/io/inspect/koreanstockinspector/tdd/price/domain/LocalDynamoPriceKrDayCRUDTest.java
@@ -1,4 +1,4 @@
-package io.inspect.koreanstockinspector.tdd.finance.repository.dynamo;
+package io.inspect.koreanstockinspector.tdd.price.domain;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -6,9 +6,8 @@ import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
-import io.inspect.koreanstockinspector.finance.domain.FinanceGainLossQuarterly;
+import io.inspect.koreanstockinspector.price.docmain.PriceKrDay;
 import io.inspect.koreanstockinspector.testutils.DockerLocalDynamoDbTestConfigUtils;
-import io.inspect.koreanstockinspector.testutils.DockerLocalDynamoDbTestConfigUtils.LocalDynamoDbEnvironment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
@@ -16,13 +15,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
-public class LocalDynamoFinanceQuarterlyCRUDTest {
+public class LocalDynamoPriceKrDayCRUDTest {
     AmazonDynamoDB amazonDynamoDB;
     DynamoDBMapper dynamoDBMapper;
 
     @BeforeEach
     public void testLocalInit(){
-        LocalDynamoDbEnvironment localDynamoDbEnvironment = DockerLocalDynamoDbTestConfigUtils.newLocalDynamoDbEnvironment();
+        DockerLocalDynamoDbTestConfigUtils.LocalDynamoDbEnvironment localDynamoDbEnvironment = DockerLocalDynamoDbTestConfigUtils.newLocalDynamoDbEnvironment();
         amazonDynamoDB = localDynamoDbEnvironment.amazonDynamoDB();
         dynamoDBMapper = localDynamoDbEnvironment.dynamoDBMapper();
     }
@@ -30,10 +29,10 @@ public class LocalDynamoFinanceQuarterlyCRUDTest {
     // AmazonDynamoDB SDK 를 사용하지 않고 DynamodbMapper 를 이용해 테이블을 생성
     @Order(1)
     @Test
-    @DisplayName("FinanceGainLossQuarterly 테이블 생성 >> spring-data-dynamodb 의 DynamodbMapper 를 이용해 테이블을 생성")
+    @DisplayName("PriceKrDay 테이블 생성 >> spring-data-dynamodb 의 DynamodbMapper 를 이용해 테이블을 생성")
     public void TEST_CREATE_TABLE_BY_ORM_MAPPER_DYNAMODBMAPPER(){
         CreateTableRequest request = dynamoDBMapper
-                .generateCreateTableRequest(FinanceGainLossQuarterly.class)
+                .generateCreateTableRequest(PriceKrDay.class)
                 .withProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
 
         then(TableUtils.createTableIfNotExists(amazonDynamoDB, request)).isTrue();
@@ -43,10 +42,9 @@ public class LocalDynamoFinanceQuarterlyCRUDTest {
     // AmazonDynamoDB SDK 를 사용하지 않고 DynamodbMapper 를 이용해 테이블을 삭제
     @Order(2)
     @Test
-    @DisplayName("FinanceGainLossYearly 테이블 삭제 >> spring-data-dynamodb 의 DynamodbMapper 를 이용해 테이블을 삭제")
+    @DisplayName("PriceKrDay 테이블 삭제 >> spring-data-dynamodb 의 DynamodbMapper 를 이용해 테이블을 삭제")
     public void TEST_DELETE_TABLE_BY_ORM_MAPPER_DYNAMOMAPPER(){
-        DeleteTableRequest deleteTableRequest = dynamoDBMapper.generateDeleteTableRequest(FinanceGainLossQuarterly.class);
+        DeleteTableRequest deleteTableRequest = dynamoDBMapper.generateDeleteTableRequest(PriceKrDay.class);
         then(TableUtils.deleteTableIfExists(amazonDynamoDB, deleteTableRequest)).isTrue();
     }
-
 }
